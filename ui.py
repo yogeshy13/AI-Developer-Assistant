b979ec50dbedd276e25632e91f14647b695340d5
Yogesh <yogesh0387@gmail.com>
2026-06-21 14:31:56 +0200

    re-factor

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 
-st.set_page_config(page_title="AI Dev Assistant", layout="wide")
+st.set_page_config(page_title="Bug0", layout="wide")
 
 # ---------- THEME ----------
 
